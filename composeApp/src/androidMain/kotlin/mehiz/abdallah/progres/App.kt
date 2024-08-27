90db87f0c01110429ac8e554e9230b30c5a58fcf
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-08-27 06:33:34 +0100

    fix: configuration changes causing kodein to re-initialize

--- composeApp/src/androidMain/kotlin/mehiz/abdallah/progres/App.kt
+++ composeApp/src/androidMain/kotlin/mehiz/abdallah/progres/App.kt
@@ -3,8 +3,20 @@ package mehiz.abdallah.progres
 import android.app.Application
 import de.halfbit.logger.initializeLogger
 import de.halfbit.logger.sink.println.registerPrintlnSink
+import di.initKodein
+import mehiz.abdallah.progres.i18n.Localize
+import org.kodein.di.DIAware
+import org.kodein.di.android.x.androidXModule
 
-class App : Application() {
+class App : Application(), DIAware {
+
+  override val di by lazy {
+    initKodein(
+      applicationContext = androidXModule(this),
+      datastorePath = applicationContext.filesDir.path,
+      localize = Localize(this),
+    )
+  }
 
   override fun onCreate() {
     super.onCreate()
--- composeApp/src/androidMain/kotlin/mehiz/abdallah/progres/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/mehiz/abdallah/progres/MainActivity.kt
@@ -6,17 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import di.initKodein
 import mehiz.abdallah.progres.i18n.Localize
+import org.kodein.di.DIAware
+import org.kodein.di.android.closestDI
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.compose.withDI
 
-class MainActivity : ComponentActivity() {
-  private val di by lazy {
-    initKodein(
-      applicationContext = androidXModule(application),
-      datastorePath = applicationContext.filesDir.path,
-      localize = Localize(this),
-    )
-  }
+class MainActivity : ComponentActivity(), DIAware {
+  override val di by closestDI(this)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
