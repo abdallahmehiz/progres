ebb2149717a42cd9baa9fa5c8eb6430afa167459
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-08-27 06:39:52 +0100

    lint

--- composeApp/src/androidMain/kotlin/mehiz/abdallah/progres/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/mehiz/abdallah/progres/MainActivity.kt
@@ -4,11 +4,8 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import di.initKodein
-import mehiz.abdallah.progres.i18n.Localize
 import org.kodein.di.DIAware
 import org.kodein.di.android.closestDI
-import org.kodein.di.android.x.androidXModule
 import org.kodein.di.compose.withDI
 
 class MainActivity : ComponentActivity(), DIAware {
