414c288e190d58db28ab9e404c1639eb4176249c
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-10-10 12:50:11 +0100

    chore: monochrome icon i hate inkscape

--- composeApp/src/commonMain/kotlin/presentation/ErrorScreenContent.kt
+++ composeApp/src/commonMain/kotlin/presentation/ErrorScreenContent.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.BugReport
-import androidx.compose.material.icons.rounded.BugReport
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
